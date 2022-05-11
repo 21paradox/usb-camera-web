ec91a1fadb4847e204e3bb80c5aa5ed54b21dcba
shiwenzhe <shiwenzhe@lixiang.com>
2022-05-11 14:16:08 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="wrap">
+    <fieldset class="constraints">
+      <legend>Video Constraints</legend>
+      <label>width: <input type="number" v-model="width" /></label>
+      <label>height: <input type="number" v-model="height" /></label>
+      <label>framerate: <input type="number" v-model="frameRate" /></label>
+    </fieldset>
+
+    <div class="divider"></div>
+    <button @click="requestCameraAuth" style="margin-right: 10px">
+      requestCameraAuth
+    </button>
+    <select v-if="deviceList.length > 0" @change="selectDevice">
+      <option v-for="(device, index) in deviceList" :key="index" :value="index">
+        {{ device.kind + ' ' + device.label }}
+      </option>
+    </select>
+    <div class="divider"></div>
+    <video v-show="showVideo" controls ref="videoElem" autoplay muted></video>
   </div>
 </template>
 
@@ -34,25 +25,67 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      deviceSelected: null,
+      deviceList: [],
+
+      width: 1920,
+      height: 1080,
+      frameRate: 60,
+      showVideo: false,
+    };
+  },
+  methods: {
+    async selectDevice(event) {
+      const device = this.deviceList[event.target.value];
+      const media = await navigator.mediaDevices.getUserMedia({
+        video: {
+          deviceId: device.deviceId,
+          width: this.width,
+          height: this.height,
+          frameRate: this.frameRate,
+        },
+      });
+      this.$refs.videoElem.srcObject = media;
+      this.showVideo = true;
+      // this.deviceSelected = devices[0];
+      // for (let device of devices) addDevice(device);
+      // selectDevice(devices[0]);
+    },
+    async requestCameraAuth() {
+      const media = navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      this.deviceList = devices;
+      console.log(devices);
+      return media;
+    },
+    async beginCapture() {
+      const media = navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      return media;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.wrap {
+  width: 800px;
+  margin: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.constraints {
+  text-align: left;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.constraints label {
+  margin-right: 10px;
 }
-a {
-  color: #42b983;
+.divider {
+  height: 20px;
 }
 </style>
